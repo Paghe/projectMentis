2fd9afa0e63fb1e1deac7275627d42930321dd6e
Paghe <alessandro.paghera99@gmail.com>
2025-10-09 21:26:36 +0200

    feat(get): added users get request

--- backend/src/api/handlers/user.go
+++ backend/src/api/handlers/user.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"net/http"
 	"time"
+	"fmt"
 	"github.com/Paghe/projectMentis/backend/src/config"
 	"github.com/Paghe/projectMentis/backend/src/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -33,4 +35,31 @@ func CreateUserHandle(c *gin.Context) {
 		"message": "user created successfully",
 		"user": user,
 	})
+}
+
+//GetUsers handles GET /users requests and returns all users in the database.
+func GetUsers(c *gin.Context) {
+	var users []models.User
+	userCollection := config.GetCollection("user")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	results, err := userCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer func() {
+		if err := results.Close(ctx); err != nil {
+			fmt.Println("❌ Error closing cursor:", err)
+		}
+	}()
+	for results.Next(ctx) {
+		var singleUser models.User
+		if err = results.Decode(&singleUser); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		users = append(users, singleUser)
+	}
+	fmt.Println("✅ Total users found:", len(users))
+	c.JSON(http.StatusOK, gin.H{"users": users})
 }

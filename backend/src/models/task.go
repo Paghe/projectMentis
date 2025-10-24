952f2c754c037071e4f30edca2a996ccb39be168
Paghe <alessandro.paghera99@gmail.com>
2025-10-25 00:12:09 +0200

    chore(task): set id for task

--- backend/src/models/task.go
+++ backend/src/models/task.go
@@ -1,13 +1,18 @@
 // Package models contains data structures for the Mentis application.
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Task represents a task in the system.
 type Task struct {
-	ID			string 				`bson:"_id,omitempty" json:"id"`
+	ID			primitive.ObjectID 	`bson:"_id,omitempty" json:"id"`
 	UserID		primitive.ObjectID	`bson:"user_id" json:"user_id"`
-	Character	primitive.ObjectID	`bson:"character_id" json:"character_id"`
-	Title     	string `json:"title" bson:"title"`
-	Completed 	bool   `json:"completed" bson:"completed"`
+	CharacterID	primitive.ObjectID	`bson:"character_id" json:"character_id"`
+	Title     	string				`bson:"title" json:"title"`
+	Completed 	bool				`bson:"completed" json:"completed"`
+	CreatedAt   time.Time			`bson:"created_at" json:"created_at"`
 }

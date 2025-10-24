9b8080e11dc77ba0211860e9921d195a9457aaff
Paghe <alessandro.paghera99@gmail.com>
2025-10-24 23:11:59 +0200

    feat(task): added relationship with user and character

--- backend/src/models/task.go
+++ backend/src/models/task.go
@@ -1,9 +1,13 @@
 // Package models contains data structures for the Mentis application.
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // Task represents a task in the system.
 type Task struct {
-	ID        string `json:"id" bson:"_id,omitempty"`
-	Title     string `json:"title" bson:"title"`
-	Completed bool   `json:"completed" bson:"completed"`
+	ID			string 				`bson:"_id,omitempty" json:"id"`
+	UserID		primitive.ObjectID	`bson:"user_id" json:"user_id"`
+	Character	primitive.ObjectID	`bson:"character_id" json:"character_id"`
+	Title     	string `json:"title" bson:"title"`
+	Completed 	bool   `json:"completed" bson:"completed"`
 }
